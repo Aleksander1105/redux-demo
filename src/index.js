4e9207345daac02282cb44ee109e6fa0a4683cc9
Aleksander Matyjewicz <matyjewiczaleksander@gmail.com>
2018-05-11 15:52:59 +0200

    Zadanie_19_7(basic)-Done!

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import reducer from './reducer'
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { addComment } from './actions.js';
 
 const store = createStore(reducer);
 
@@ -13,3 +15,6 @@ ReactDOM.render(
 	</Provider>,
 	document.getElementById('root')
 );
+
+store.dispatch(addComment('pierwszy komentarz'));
+store.dispatch(addComment('drugi komentarz'));
